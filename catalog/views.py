cccd733155449bed90f2b1be53011aa032ea3a18
Viktor Golovkov <deynekochan@mail.ru>
2024-08-26 21:57:04 +0300

    hw3

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
-def home(request):
-    return render(request, "home.html")
+def product_list(request):
+    product = (Product.objects.all())
+    context = {"product": product}
+    return render(request, "product_list.html", context)
 
 
-def contacts(request):
-    return render(request, "contacts.html")
+def product_detail(request, pk):
+    product_details = get_object_or_404(Product, pk=pk)
+    context = {"product_details": product_details}
+    return render(request, "product_detail.html", context)
+
+
