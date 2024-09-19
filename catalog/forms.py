48ae543e505335d42e9e734e5466f0ed70668044
Viktor Golovkov <deynekochan@mail.ru>
2024-09-19 11:39:51 +0300

    HW forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -7,11 +7,11 @@ from catalog.models import Product, Version
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            if isinstance(field, BooleanField):
-                field.widget.attrs["class"] = "form-check-input"
+        for fild_name, fild in self.filds.items():
+            if isinstance(fild, BooleanField):
+                fild.widget.attrs["class"] = "form-check-input"
             else:
-                field.widget.attrs["class"] = "form-control"
+                fild.widget.attrs["class"] = "form-control"
 
 
 class ProductForm(StyleFormMixin, ModelForm):
